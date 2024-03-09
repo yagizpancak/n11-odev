56f85d4fdd621287dd3c5b82dacbb45c7df4a6cc
yagizpancak <yagizcan923@gmail.com>
2024-03-09 17:50:06 +0300

    initial commit

---
+++ odev-3/src/main/java/com/n11/odev3/Odev3Application.java
@@ -0,0 +1,13 @@
+package com.n11.odev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Odev3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Odev3Application.class, args);
+	}
+
+}
---
+++ odev-3/src/test/java/com/n11/odev3/Odev3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n11.odev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Odev3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
